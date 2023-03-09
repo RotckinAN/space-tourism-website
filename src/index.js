316a5414d39b9a2d73bc98f817e5f70374c2486d
Anatolii Rotckin <rokkker@yandex.ru>
2023-03-09 17:51:28 +0200

    fix: исправить basename в роутере в index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
 
+//! Задать базовый путь для роутера (поменять в .env базовый путь при необходимости)
+const baseName =
+   process.env.NODE_ENV !== 'production'
+      ? '/'
+      : process.env.REACT_APP_HOME_PAGE;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-   // <BrowserRouter basename="/movies-explorer-frontend"> /// Для деплоя
-   <BrowserRouter>
+   <BrowserRouter basename={baseName}>
       <React.StrictMode>
          <App />
       </React.StrictMode>
